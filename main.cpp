22e37a6642df712e30d7878ab0e8192de6be25c2
Andrewliera <47090507+Andrewliera@users.noreply.github.com>
2019-02-07 18:47:07 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i;
+    cout << "please enter an integer value: ";
+    cin >> i;
+    cout << "the Value you entered is " << i;
+    cout << " and its double is " << i*2 << " .\n ";
+
+}
